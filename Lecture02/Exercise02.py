ba28c1d06224cc310029219c569c0f6c4c3d59df
Nicklas Nyegaard <nnyegaard@gmail.com>
2013-02-10 23:36:52 +0100

    1. Added convection of response data intro normal json 2. Added tags and bbox to match exercise

--- Lecture02/Exercise02.py
+++ Lecture02/Exercise02.py
@@ -1,13 +1,14 @@
 __author__ = 'nnyegaard'
 
 import flickrapi
+import json
 
 api_key = "43f96ceebd1427512a63ae71266e54ca"
 
-
-flickr = flickrapi.FlickrAPI(api_key)
-
-photos = flickr.photos_search(tags=['Monty', 'Python'], format='json')
+flickr = flickrapi.FlickrAPI(api_key, cache=True)
 
 
-print photos
+photos = flickr.photos_search(tags=["Denmark"], format='json', bbox=[8, 13, 55, 58], page=2)
+jdata = json.loads(photos[14:-1])
+
+print json.dumps(jdata, separators = (',' , ':'), indent = 4)
