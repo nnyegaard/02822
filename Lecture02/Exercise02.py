e5cebce38cd280254bc6bc658e8ff77a74744c8b
Nicklas Nyegaard <nnyegaard@gmail.com>
2013-02-11 23:14:30 +0100

    Saved data methods.

--- Lecture02/Exercise02.py
+++ Lecture02/Exercise02.py
@@ -7,8 +7,26 @@ api_key = "43f96ceebd1427512a63ae71266e54ca"
 
 flickr = flickrapi.FlickrAPI(api_key, cache=True)
 
-
 photos = flickr.photos_search(tags=["Denmark"], format='json', bbox=[8, 13, 55, 58], page=2)
 jdata = json.loads(photos[14:-1])
 
-print json.dumps(jdata, separators=(',', ':'), indent = 4)
+print "Saved our raw json data with pprint."
+
+try:
+
+    with open("rawjsondata.txt", "w") as f:
+        f.write(json.dumps(jdata, separators=(',', ':'), indent=4))
+
+except IOError as e:
+    print e
+
+
+print "Saved only the photo list of the json data."
+
+try:
+
+    with open("photolist.txt", "w") as f:
+        f.write(json.dumps(jdata["photos"]["photo"], separators=(',', ':'), indent=4))
+
+except IOError as e:
+    print e
