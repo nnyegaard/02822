e8d508fe1caa60cf7d28290e9a4e36606d4e1483
Nicklas Nyegaard <nnyegaard@gmail.com>
2013-02-12 11:45:39 +0100

    Færdig gjord ex1 og 2
    Sammen med Mads Frandsen

--- Lecture02/Exercise01.py
+++ Lecture02/Exercise01.py
@@ -1,13 +1,68 @@
 __author__ = 'nnyegaard'
 
-from matplotlib.pyplot import plot
+import matplotlib.pyplot as plt
+import numpy as np
+import operator
 
 
 with open("alcohol.txt", "r") as f:
     data = f.readlines()
 
-pdata = []
+lande = []
+alco = []
+samlet = {}
+sorted_lande = []
+sorted_alco = []
 
-for e in data:
-    pdata.append(e.rstrip("\n").split("|"))
+#Generate list of countries, alcohol %, and a dic of the 2 types
+for x in data:
+    lande.append(x.split("|")[0])
+    alco.append(float(x.split("|")[1].rstrip("\n")))
+    samlet[x.split("|")[0]] = float(x.split("|")[1].rstrip("\n"))
+
+
+#Sort our dic -- Will turn it into a list of tuples
+sorted_x = sorted(samlet.iteritems(), key=operator.itemgetter(1), reverse=True)
+
+for x in sorted_x:
+    sorted_lande.append(x[0])
+    sorted_alco.append(x[1])
+
+
+def showData():
+    print "Our country list is: %r" % lande
+    print "Our list of alcohol: %r" % alco
+    print "Our dic of the 2 types: %r" % samlet
+    print "Our sorted by alcohol list of tuples: %r" % sorted_x
+    print "Our sorted countries: %r" % sorted_lande
+    print "Our sorted alcohol: %r" % sorted_alco
+
+
+def linePlot():
+    plt.xticks(np.arange(len(sorted_lande)), sorted_lande, rotation=90)
+    plt.plot(sorted_alco)
+    plt.show()
+
+
+def barPlot():
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.bar(np.arange(len(alco)), alco, width=10.50, color='r')
+    plt.show()
+
+
+def newBarPlot():
+    empty = ["", "M countries", ""]
+    fig = plt.figure()
+    plt.xticks(np.arange(33), sorted_lande[0:15] + empty + sorted_lande[-16:-1], rotation=80)
+    ax = fig.add_subplot(111)
+    ax.bar(np.arange(33), sorted_alco[0:15] + [0, 0, 0] + sorted_alco[-16:-1], width=1, color='b')
+    plt.show()
+
+
+if __name__ == '__main__':
+    #showData()
+    #linePlot()
+    newBarPlot()
+
 
--- Lecture02/Exercise02.py
+++ Lecture02/Exercise02.py
@@ -7,26 +7,53 @@ api_key = "43f96ceebd1427512a63ae71266e54ca"
 
 flickr = flickrapi.FlickrAPI(api_key, cache=True)
 
-photos = flickr.photos_search(tags=["Denmark"], format='json', bbox=[8, 13, 55, 58], page=2)
-jdata = json.loads(photos[14:-1])
 
-print "Saved our raw json data with pprint."
+def getPhotos():
+    #bbox gives 250 per page, so 4x250 = 1000 or 2x500
+    photos_sea = flickr.photos_search(tags=['beach', 'sea', 'water', 'vand', 'havet'], format='json',
+                                      bbox=[8, 55, 13, 58],
+                                      page=4)
+    jdata_sea = json.loads(photos_sea[14:-1])
+
+    photos_inland = flickr.photos_search(tags=['forest', 'nature', 'grass', 'graes', 'skov'], format='json',
+                                         bbox=[8, 55, 13, 58], page=4)
+    jdata_inland = json.loads(photos_inland[14:-1])
+
+    print "Saved our raw json data with pprint."
+
+    try:
+        with open("rawjsondata_sea.txt", "w") as f:
+            f.write(json.dumps(jdata_sea, separators=(',', ':'), indent=4))
+
+        with open("rawjsondata_inland.txt", "w") as f:
+            f.write(json.dumps(jdata_inland, separators=(',', ':'), indent=4))
+
+    except IOError as e:
+        print e
+
+    print "Saved only the photo list of the json data."
+
+    try:
+        with open("photolist_sea.txt", "w") as f:
+            f.write(json.dumps(jdata_sea["photos"]["photo"], separators=(',', ':'), indent=4))
+
+        with open("photolist_inland.txt", "w") as f:
+            f.write(json.dumps(jdata_inland["photos"]["photo"], separators=(',', ':'), indent=4))
+
+    except IOError as e:
+        print
+
+        #print len(jdata_sea["photos"]["photo"])
+
 
 try:
-
-    with open("rawjsondata.txt", "w") as f:
-        f.write(json.dumps(jdata, separators=(',', ':'), indent=4))
+    with open("photolist_sea.txt", "r") as f:
+        seaPhotos = f.read()
 
 except IOError as e:
     print e
 
+for x in json.loads(seaPhotos):
+    print flickr.photos_geo_getLocation(photo_id=x["id"], format='json')[14:-1]
 
-print "Saved only the photo list of the json data."
-
-try:
-
-    with open("photolist.txt", "w") as f:
-        f.write(json.dumps(jdata["photos"]["photo"], separators=(',', ':'), indent=4))
-
-except IOError as e:
-    print e
+#print flickr.photos_geo_getLocation(photo_id=8143819572, format='json')
